b3b230435b7a4154042b5e49dab149708848607e
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-06-03 08:05:31 +0300

    working algorithm

--- frb/search_utils.py
+++ frb/search_utils.py
@@ -5,6 +5,9 @@ import numpy as np
 path = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
 sys.path.insert(0, path)
 from frb.utils import delta_dm_max
+from frb.frames import vint
+from scipy.ndimage.morphology import generate_binary_structure
+from scipy.ndimage.measurements import find_objects, label, maximum_position
 try:
     import matplotlib.pyplot as plt
 except ImportError:
@@ -12,43 +15,43 @@ except ImportError:
 
 
 def search_frame(frame, dm_min, dm_max, savefig=None):
-    dm_delta = delta_dm_max(frame.nu_0, frame.nu_0 - frame.n_nu * frame.dnu,
+    dm_delta = 5 * delta_dm_max(frame.nu_0, frame.nu_0 - frame.n_nu * frame.dnu,
                             frame.dt)
 
     n_dm = (dm_max - dm_min) / dm_delta
     n_dm = int(math.ceil(n_dm))
+    snr_ts= list()
     max_snrs = list()
     dm_used = list()
+    frames_dedm = list()
     for i in xrange(n_dm):
         dm = dm_min + (i + 1) * dm_delta
         print "Searching DM = ", dm, " cm^3 / pc"
         _frame = frame.de_disperse(dm=dm, replace=False)
         _frame_t = frame.average_in_freq(_frame)
-        max_snr = max(abs((_frame_t - np.mean(_frame_t)) / np.std(_frame_t)))
-        print "Max SNR = ", max_snr
-        max_snrs.append(max_snr)
+        frames_dedm.append(_frame_t)
+        snr_t = (_frame_t - np.mean(_frame_t)) / np.std(_frame_t)
+        snr_t_ = snr_t[:]
+        snr_t_[snr_t_ < 0] = 0.
+        snr_ts.append(snr_t)
+        max_snrs.append(max(snr_t_))
         dm_used.append(dm)
 
-    dm_result = np.mean(np.array(dm_used)[np.where(np.array(max_snrs) ==
-                                                   max(max_snrs))])
-    print "DM = ",  dm_result
+    snr_ts = np.vstack(snr_ts)
+    max_snrs = np.vstack(max_snrs)
+    dm_used = np.array(dm_used)
+    frames_dedm = np.array(frames_dedm)
+
+    return dm_used, snr_ts, max_snrs, frames_dedm
 
     # Print and (optionally) plot results
-    plt.close()
     plt.figure()
-    plt.subplot(211)
-    plt.plot(dm_used, max_snrs, '.k')
-    plt.xlabel("DM-correction")
-    plt.ylabel("max SNR")
-    plt.axvline(dm_result, color='r', lw=3, label=str(round(dm_result, 1)) +
-                                                  " cm^3 / pc")
-    plt.legend(loc='best')
-    plt.subplot(212)
-    values = frame.de_disperse(dm=dm_result)
-    plt.imshow(values, interpolation='none', aspect='auto', cmap=plt.cm.Reds)
+    plt.imshow(snr_ts, interpolation='none', aspect='auto')
     plt.colorbar()
     plt.xlabel("time steps")
-    plt.ylabel("frequency ch. #")
+    plt.ylabel("dispersion measure")
+    plt.yticks(np.linspace(0, len(dm_used) - 1, 5, dtype=int),
+               vint(dm_used[np.linspace(0, len(dm_used) - 1, 5, dtype=int)]))
     plt.show()
     if savefig is not None:
         plt.savefig(savefig, bbox_inches='tight')
@@ -56,9 +59,64 @@ def search_frame(frame, dm_min, dm_max, savefig=None):
 
 
 if __name__ == '__main__':
-    from frb.frames import DataFrame
+    from frb.frames import DataFrame, SimFrame, vint
     fname = '/home/ilya/code/frb/data/data.txt'
+    # pulses at t=0.2 and 0.65 with DM=480
     frame = DataFrame(fname, 1684., 0., 16. / 32., 0.001)
-    frame.add_pulse(0.4, 0.1, 0.0015, dm=700.)
-    frame.add_noise(0.10)
-    search_frame(frame, 0, 1000.)
+    # frame = SimFrame(128, 1000, 1676., 0., 16. / 128., 0.001)
+    frame.add_pulse(0.3, 0.05, 0.0015, dm=200.)
+    frame.add_pulse(0.9, 0.1, 0.003, dm=700.)
+    plt.close()
+    plt.imshow(frame.values, interpolation='none', aspect='auto')
+    plt.xlabel('Time')
+    plt.ylabel('Freq. channel')
+    plt.colorbar()
+    plt.savefig('pulse_clean1.png')
+    frame.add_noise(0.1)
+    plt.close()
+    plt.imshow(frame.values, interpolation='none', aspect='auto')
+    plt.xlabel('Time')
+    plt.ylabel('Freq. channel')
+    plt.colorbar()
+    plt.savefig('pulse_dirty1.png')
+    dm_used, snr_ts, max_snrs, frames_dedm = search_frame(frame, 0, 1000.)
+    plt.close()
+    plt.imshow(frames_dedm, interpolation='none', aspect='auto')
+    plt.xlabel('De-dispersed by DM freq.averaged frame')
+    plt.ylabel('DM correction')
+    plt.yticks(np.linspace(0, len(dm_used) - 10, 5, dtype=int),
+               vint(dm_used[np.linspace(0, len(dm_used) - 10, 5, dtype=int)]))
+    plt.colorbar()
+    plt.savefig('stripes1.png')
+
+
+    # Now find stripes in "DM-correction vs. freq.averaged" plane
+    threshold=np.percentile(frames_dedm.flatten(), 98)
+    a = frames_dedm.copy()
+    # Keep only tail of distribution with signal (and correlated noise:)
+    a[a < threshold] = 0
+    s = generate_binary_structure(2,2)
+    # Label image
+    labeled_array, num_features = label(a, structure=s)
+
+    objects = find_objects(labeled_array)
+    # Find only objects that are elongated along DM axis
+    dm_objects = list()
+    for obj in objects:
+        freq_slice = obj[0]
+        if int(freq_slice.stop - freq_slice.start) > 50:
+            dm_objects.append(obj)
+
+    # Find what labels number corresponds to such objects
+    n_labels = list()
+    for obj in dm_objects:
+        # Counts of all labels but zero
+        counts = np.bincount(labeled_array[obj].flatten())[1:]
+        n_label = int(np.where(counts == max(counts))[0] + 1)
+        n_labels.append(n_label)
+
+    #center_of_mass(frames_dedm, labeled_array, n_labels)
+    pos = maximum_position(frames_dedm, labels=labeled_array, index=n_labels)
+    print 'Found ', len(n_labels), ' pulses'
+    for i in range(len(n_labels)):
+        print "at time ", pos[i][1] / 1000., ' with DM = ', dm_used[pos[i][0]]
