fd040d1875f8c6e457930360c10c39c844ac5601
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-06-11 11:07:54 +0300

    with new objects

--- bin/search_file.py
+++ bin/search_file.py
@@ -4,7 +4,7 @@ import argparse
 path = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
 sys.path.insert(0, path)
 from frb.frames import DataFrame
-from frb.search_utils import grid_dedisperse_frame, find_pulses
+from frb.objects import TDMImageObjects
 try:
     import matplotlib.pyplot as plt
 except ImportError:
@@ -31,12 +31,22 @@ if __name__ == '__main__':
                         help='- mininum DM to search')
     parser.add_argument('-dm_max', action='store', dest='dm_max', type=float,
                         help='- maxmum DM to search')
+    parser.add_argument('-perc', action='store', dest='perc', default=99.95,
+                        type=float, help='- percent of image to blank')
+    parser.add_argument('-d_dm', action='store', dest='d_dm', default=150.,
+                        type=float, help='- width of feature [cm^3/pc] in'
+                                         '(t, DM)-space along DM-axis to treat'
+                                         'as candidate')
+    parser.add_argument('-d_t', action='store', dest='d_t', default=0.005,
+                        type=float, help='- width of feature [s] in'
+                                         '(t, DM)-space along DM-axis to treat'
+                                         'as candidate')
     parser.add_argument('-savefig_dyn', action='store', nargs='?',
                         default=None, type=str, metavar='path to file',
                         help='- file to save dyn.spectr plot')
     parser.add_argument('-savefig_dedm', action='store', nargs='?',
                         default=None, type=str, metavar='path to file',
-                        help='- file to save de-DM freq.averaged dyn. spectra')
+                        help='- file to save de-DM freq. averaged dyn. spectra')
 
     args = parser.parse_args()
 
@@ -45,6 +55,8 @@ if __name__ == '__main__':
     dm_min = args.dm_min
     dm_max = args.dm_max
 
-    dm_grid, frames_t_dedm = grid_dedisperse_frame(frame, dm_min, dm_max,
+    dm_grid, frames_t_dedm = frame.grid_dedisperse(dm_min, dm_max,
                                                    savefig=args.savefig_dedm)
-    find_pulses(dm_grid, frames_t_dedm)
+    candidates = TDMImageObjects(frames_t_dedm, frame.t, dm_grid,
+                                 perc=args.perc, d_dm=args.d_dm, dt=args.d_t)
+    candidates.save_txt('candidates.txt', 'x', 'y')
