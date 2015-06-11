1b2a8742b56033e58a1723c517d4ffbf6d358b30
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-06-11 14:18:26 +0300

    update script

--- bin/search_file.py
+++ bin/search_file.py
@@ -13,40 +13,50 @@ except ImportError:
 
 if __name__ == '__main__':
     parser =\
-        argparse.ArgumentParser(description='Search dispersed signals in'
+        argparse.ArgumentParser(description='Search for dispersed signals in'
                                 'dynamic spectra')
 
     parser.add_argument('fname', type=str,
                         metavar='txt-file with dynamical spectra',
                         help='- path to file with dynamical spectra')
     parser.add_argument('-nu_max', action='store', dest='nu_max', type=float,
-                        help='- frequency of highest freq. channel')
+                        help='- frequency of highest freq. channel [MHz]')
     parser.add_argument('-t0', action='store', dest='t0', default=0.0,
-                        type=float, help='- beginning time')
+                        type=float, help='- beginning time [s].'
+                                         'Default: 0.0')
     parser.add_argument('-dnu', action='store', dest='dnu', type=float,
-                        help='- frequency width of each channel')
+                        help='- frequency width of each channel [MHz]')
     parser.add_argument('-dt', action='store', dest='dt', type=float,
-                        help='- time resolution')
+                        help='- time resolution [s]')
     parser.add_argument('-dm_min', action='store', dest='dm_min', type=float,
-                        help='- mininum DM to search')
+                        help='- mininum DM to search [cm^3/pc]')
     parser.add_argument('-dm_max', action='store', dest='dm_max', type=float,
-                        help='- maxmum DM to search')
-    parser.add_argument('-perc', action='store', dest='perc', default=99.95,
-                        type=float, help='- percent of image to blank')
-    parser.add_argument('-d_dm', action='store', dest='d_dm', default=150.,
+                        help='- maxmum DM to search [cm^3/pc]')
+    parser.add_argument('-perc', action='store', dest='perc', default=99.5,
+                        type=float, help='- percentile of image values that is'
+                                         ' used to blank image before'
+                                         ' searching for objects.'
+                                         ' Default: 99.5')
+    parser.add_argument('-d_dm', action='store', dest='d_dm', default=100.,
                         type=float, help='- width of feature [cm^3/pc] in'
                                          '(t, DM)-space along DM-axis to treat'
-                                         'as candidate')
+                                         ' it as candidate.'
+                                         ' Default: 100.')
     parser.add_argument('-d_t', action='store', dest='d_t', default=0.005,
                         type=float, help='- width of feature [s] in'
                                          '(t, DM)-space along DM-axis to treat'
-                                         'as candidate')
+                                         ' it as candidate.'
+                                         ' Default: 0.005')
     parser.add_argument('-savefig_dyn', action='store', nargs='?',
                         default=None, type=str, metavar='path to file',
                         help='- file to save dyn.spectr plot')
     parser.add_argument('-savefig_dedm', action='store', nargs='?',
                         default=None, type=str, metavar='path to file',
-                        help='- file to save de-DM freq. averaged dyn. spectra')
+                        help='- file to save dedispersioned freq. averaged dyn.'
+                             ' spectra')
+    parser.add_argument('-save_result', action='store', nargs='?',
+                        default=None, type=str, metavar='path to file',
+                        help='- file to save (t, DM)-coordinates of candidates')
 
     args = parser.parse_args()
 
@@ -59,4 +69,4 @@ if __name__ == '__main__':
                                                    savefig=args.savefig_dedm)
     candidates = TDMImageObjects(frames_t_dedm, frame.t, dm_grid,
                                  perc=args.perc, d_dm=args.d_dm, dt=args.d_t)
-    candidates.save_txt('candidates.txt', 'x', 'y')
+    candidates.save_txt(args.save_result, 'x', 'y')
